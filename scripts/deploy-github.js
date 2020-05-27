444c29314995c4d812b1ac1cb39bef2ca908a156
Jeremie <jeremiegthr@gmail.com>
2020-05-27 11:33:14 +0200

    https -> ssh

--- scripts/deploy-github.js
+++ scripts/deploy-github.js
@@ -4,7 +4,7 @@ ghpages.publish(
 	"public",
 	{
 		branch: "master",
-		repo: "https://github.com/jeremie-gauthier/jeremie-gauthier.github.io.git",
+		repo: "git@github.com:jeremie-gauthier/jeremie-gauthier.github.io.git",
 	},
 	() => {
 		console.log("Deploy Complete!");
