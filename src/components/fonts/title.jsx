6d781337b8681fe2e06f27de852f73336f26c0eb
Jeremie <jeremiegthr@gmail.com>
2020-05-28 10:15:22 +0200

    more content

--- src/components/fonts/title.jsx
+++ src/components/fonts/title.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import stylesTitle from "./title.module.css";
 
-export default ({ children }) => {
-	return <h2 className={stylesTitle.title}>{children}</h2>;
+export default ({ id, children }) => {
+	return (
+		<h2 id={id} className={stylesTitle.title}>
+			{children}
+		</h2>
+	);
 };
