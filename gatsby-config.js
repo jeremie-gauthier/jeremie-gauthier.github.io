0e143bf13d58244aca53f63679e6b406015cad1f
Jeremie <jeremiegthr@gmail.com>
2020-05-29 13:52:50 +0200

    improve accessibility

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,12 @@ const path = require(`path`);
 module.exports = {
 	plugins: [
 		{
+			resolve: "gatsby-plugin-html-attributes",
+			options: {
+				lang: "fr",
+			},
+		},
+		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
 				name: `assets`,
@@ -11,5 +17,6 @@ module.exports = {
 		},
 		`gatsby-transformer-sharp`,
 		`gatsby-plugin-sharp`,
+		`gatsby-plugin-react-helmet`,
 	],
 };
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Helmet } from "react-helmet";
 import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/global.css";
@@ -11,6 +12,14 @@ export default () => {
 
 	return (
 		<Container fluid>
+			<Helmet>
+				<link
+					rel="icon"
+					href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤖</text></svg>"
+				/>
+				<title>J&eacute;r&eacute;mie GAUTHIER</title>
+			</Helmet>
+
 			{/* Show on xs only */}
 			<Row className="d-block d-sm-none bg-header">
 				<HeaderMobile activeLink={activeLink} setActiveLink={setActiveLink} />
