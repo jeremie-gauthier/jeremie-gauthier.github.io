2ba59040ea7ff72295944852523e20f3b51824fb
Jeremie <jeremiegthr@gmail.com>
2020-05-29 19:06:53 +0200

    change style

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import stylesSubpages from "./subpages.module.css";
 import stylesAbout from "./about.module.css";
 import react from "../assets/react.svg";
@@ -10,39 +10,49 @@ import mysql from "../assets/mysql.svg";
 export default () => {
 	return (
 		<Container className={stylesSubpages.container}>
-			<h2 className={stylesSubpages.title}>Qui suis-je ?</h2>
-			<p className={stylesSubpages.subtitle}>
-				Actuellement étudiant à 42 Paris, je suis également développeur
-				FullStack en Freelance.
-			</p>
+			<div className={stylesSubpages.marginBot}>
+				<h2 className={stylesSubpages.title}>Qui suis-je ?</h2>
+				<p className={stylesSubpages.subtitle}>
+					Actuellement étudiant à 42 Paris, je suis également développeur
+					FullStack en Freelance.
+				</p>
+			</div>
 
-			<h3 style={{ textAlign: "center" }}>Mes services</h3>
-			<div className={stylesAbout.services}>
-				<div>
+			<h3
+				className={[stylesSubpages.marginBot, stylesAbout.titleServices].join(
+					" "
+				)}
+			>
+				Mes services
+			</h3>
+			<Row
+				className={[stylesAbout.services, stylesSubpages.marginBot].join(" ")}
+			>
+				<Col>
 					<h4>FRONT-END</h4>
 					<p>
 						Je rends votre site utilisable sur tous les appareils et pour tous
-						les utilisateurs. J'aime travailler avec React, Gatsby.
+						les utilisateurs. J'aime travailler avec React et Gatsby.
 					</p>
 					<div className={stylesAbout.icons}>
 						<img src={react} title="ReactJS" alt="Logo ReactJS" />
 						<img src={gatsby} title="GatsbyJS" alt="Logo GatsbyJS" />
 					</div>
-				</div>
-				<div>
+				</Col>
+				<Col>
 					<h4>BACK-END</h4>
 					<p>
 						Je conçois votre base de données et construis la logique de votre
-						site. Pour cela, j'utilise Node, Express, MySQL.
+						site. Pour cela, j'utilise Node, Express et MySQL.
 					</p>
 					<div className={stylesAbout.icons}>
 						<img src={node} title="NodeJS" alt="Logo NodeJS" />
 						<img src={mysql} title="MySQL" alt="Logo MySQL" />
 					</div>
-				</div>
-			</div>
+				</Col>
+			</Row>
 
-			<div className={stylesAbout.quote}>
+			<div className={[stylesAbout.quote, stylesSubpages.marginBot].join(" ")}>
 				<span className={stylesAbout.quoteSymbol}>&#8223;</span>
 				<div className={stylesAbout.quoteText}>
 					<p className={stylesAbout.firstLine}>
