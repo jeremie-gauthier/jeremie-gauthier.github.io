76ecc205810bdcdc97acbc9659ae98258fccef2d
Jeremie <jeremiegthr@gmail.com>
2020-05-29 14:07:17 +0200

    add description for SEO

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,7 +17,11 @@ export default () => {
 					rel="icon"
 					href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🤖</text></svg>"
 				/>
-				<title>J&eacute;r&eacute;mie GAUTHIER</title>
+				<meta
+					name="description"
+					content="Jeremie GAUTHIER's portfolio. FullStack Developer Freelance. Currently studying at 42 Paris."
+				/>
+				<title>J&eacute;r&eacute;mie GAUTHIER - FullStack Developer</title>
 			</Helmet>
 
 			{/* Show on xs only */}
