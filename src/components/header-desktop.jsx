415da030a98186d0ad0e165624f4c0e0092236be
Jeremie <jeremiegthr@gmail.com>
2020-06-04 10:05:12 +0200

    add animation

--- src/components/header-desktop.jsx
+++ src/components/header-desktop.jsx
@@ -44,6 +44,63 @@ const Links = ({ activeLink, setActiveLink }) => {
 	));
 };
 
+const Bubbles = memo(() => (
+	<Particles
+		className={stylesHeader.bubbles}
+		params={{
+			particles: {
+				number: {
+					value: 75,
+					density: {
+						enable: false,
+					},
+				},
+				size: {
+					value: 3,
+					random: true,
+					anim: {
+						speed: 2,
+						size_min: 0.3,
+					},
+				},
+				line_linked: {
+					enable: false,
+				},
+				move: {
+					random: true,
+					speed: 1,
+					direction: "bottom",
+					out_mode: "out",
+				},
+			},
+			interactivity: {
+				events: {
+					onhover: {
+						enable: true,
+						mode: "bubble",
+					},
+					onclick: {
+						enable: true,
+						mode: "repulse",
+					},
+				},
+				modes: {
+					bubble: {
+						distance: 25,
+						duration: 1,
+						size: 0,
+						opacity: 0,
+					},
+					repulse: {
+						distance: 100,
+						duration: 2,
+					},
+				},
+			},
+		}}
+	/>
+));
+
 const Header = ({ activeLink, setActiveLink }) => {
 	return (
 		<header>
