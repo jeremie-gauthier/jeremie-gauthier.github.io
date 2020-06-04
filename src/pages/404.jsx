517f0c763b486456ea2ab1d85f86a971ff113fea
Jeremie <jeremiegthr@gmail.com>
2020-06-04 10:13:28 +0200

    change to remove single-used component

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-import Layout from "../components/layout";
+import { Container } from "react-bootstrap";
 import { Link } from "gatsby";
 
 export default () => {
 	return (
-		<Layout>
+		<Container
+			style={{ display: "flex", flexDirection: "column", textAlign: "center" }}
+		>
 			<h1>Page introuvable</h1>
 			<p>Oups, la page que vous recherchez n'existe pas.</p>
 			<Link to="/" style={{ color: "#fff" }}>
 				Retour à l'accueil
 			</Link>
-		</Layout>
+		</Container>
 	);
 };
