3095ba176e61ded12492f7198aa2777d66e6b04a
Jeremie <jeremiegthr@gmail.com>
2020-06-04 10:14:28 +0200

    remove component

--- src/components/layout.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Container } from "react-bootstrap";
-import stylesLayout from "./layout.module.css";
-
-export default ({ children }) => (
-	<Container className={stylesLayout.layout}>{children}</Container>
-);
