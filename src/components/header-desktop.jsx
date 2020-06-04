b084b6eaa675e7aaa5d249631937beeefc7301f9
Jeremie <jeremiegthr@gmail.com>
2020-06-04 10:05:36 +0200

    add animation

--- src/components/header-desktop.jsx
+++ src/components/header-desktop.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import stylesHeader from "./header-desktop.module.css";
+import Particles from "react-particles-js";
 import { StaticQuery, graphql } from "gatsby";
 import Img from "gatsby-image";
 import { Button } from "react-bootstrap";
@@ -43,63 +44,6 @@ const Links = ({ activeLink, setActiveLink }) => {
 		</Button>
 	));
 };
-
-const Bubbles = memo(() => (
-	<Particles
-		className={stylesHeader.bubbles}
-		params={{
-			particles: {
-				number: {
-					value: 75,
-					density: {
-						enable: false,
-					},
-				},
-				size: {
-					value: 3,
-					random: true,
-					anim: {
-						speed: 2,
-						size_min: 0.3,
-					},
-				},
-				line_linked: {
-					enable: false,
-				},
-				move: {
-					random: true,
-					speed: 1,
-					direction: "bottom",
-					out_mode: "out",
-				},
-			},
-			interactivity: {
-				events: {
-					onhover: {
-						enable: true,
-						mode: "bubble",
-					},
-					onclick: {
-						enable: true,
-						mode: "repulse",
-					},
-				},
-				modes: {
-					bubble: {
-						distance: 25,
-						duration: 1,
-						size: 0,
-						opacity: 0,
-					},
-					repulse: {
-						distance: 100,
-						duration: 2,
-					},
-				},
-			},
-		}}
-	/>
-));
 
 const Header = ({ activeLink, setActiveLink }) => {
 	return (
